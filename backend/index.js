b23bd674c36a28dce721c384588b4d2c186b3393
MajidAlamoudi <majidamoudi15@gmail.com>
2023-11-13 14:12:44 +0400

    First Commit
    First Date: 13/11/2023

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-Wg8zuPC4VvvTscMwyyDgiyoK",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a chatbot for in5 in Dubai. You answer any question related to in5",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
